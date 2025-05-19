fe6835893c529ff825fa0ffb4f8ec4eba7f7ea6d
DEBUG <demoorbug@gmail.com>
2025-05-19 15:49:43 +0800

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-const repoName = 'blogn' // 例如：my-next-app
-
 module.exports = {
-  output: 'export', // 启用静态导出
-  basePath: isProd ? `/${repoName}` : '', // 生产环境路径前缀
-  assetPrefix: isProd ? `/${repoName}/` : '', // 资源路径前缀
-}
+    output: 'export',
+    basePath: '/blogn', // 替换为您的仓库名称
+    images: {
+      unoptimized: true,
+    },
+  };
