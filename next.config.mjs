6f3c69ef24a96434ae8ef670eadc9df0da6da71e
DEBUG <demoorbug@gmail.com>
2025-05-19 15:46:43 +0800

    image fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'blogn' // 例如：my-next-app
 
-export default nextConfig;
+module.exports = {
+  output: 'export', // 启用静态导出
+  basePath: isProd ? `/${repoName}` : '', // 生产环境路径前缀
+  assetPrefix: isProd ? `/${repoName}/` : '', // 资源路径前缀
+}
